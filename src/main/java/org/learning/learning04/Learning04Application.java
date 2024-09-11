d99504937376df6feed1617c5da7b4f309561b0a
sayeedurrahman <enkrah@yahoo.com>
2024-09-10 21:38:53 -0700

    01 commit

--- src/main/java/org/learning/learning04/Learning04Application.java
+++ src/main/java/org/learning/learning04/Learning04Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Learning04Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Learning04Application 01");
+        System.out.println("Hello from Learning04Application 02");
         SpringApplication.run(Learning04Application.class, args);
     }
 
