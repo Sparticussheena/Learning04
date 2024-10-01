5f59ba526d43c13c4631f7eeaa37c1cbe60d6147
Sparticussheena <163904412+Sparticussheena@users.noreply.github.com>
2024-10-01 00:45:35 -0400

    Update Learning04Application.java
    version 6

--- src/main/java/org/learning/learning04/Learning04Application.java
+++ src/main/java/org/learning/learning04/Learning04Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Learning04Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Learning04Application 05");
+        System.out.println("Hello from Learning04Application 06");
         SpringApplication.run(Learning04Application.class, args);
     }
 
